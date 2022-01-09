aa346099d0ec03ad108a7f2e2c030c134c5da7aa
Erwan Coubel <erwan.f.coubel@gmail.com>
2022-01-09 10:25:58 +0100

    Test d'inscription réussis

--- src/Controller/UsersAccessController.php
+++ src/Controller/UsersAccessController.php
@@ -4,6 +4,9 @@ namespace App\Controller;
 
 use App\Entity\Users;
 use App\Form\RegisterType;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use  Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -27,10 +30,18 @@ class UsersAccessController extends AbstractController
     }
 
     #[Route('/register', name: 'users_register')]
-    public function indexRegister(): Response
+    public function indexRegister(Request $request, EntityManagerInterface $em ): Response
     {
         $utilisateur = new Users();
         $form = $this->createForm(RegisterType::class, $utilisateur );
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $em->persist($utilisateur);
+            $em->flush();
+            return $this->redirectToRoute("users_access");
+        };
+
         return $this->render('users_access/register.html.twig', [
             "form" => $form->createView()
         ]);
